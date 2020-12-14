89f925cfc6ccd632f00adb4ec2c9fb41e41f6e76
kseniya <kozlovakseniya97@gmsil.com>
2020-12-14 11:25:21 +0300

    create

---
+++ Leads sources script.sql
@@ -0,0 +1,79 @@
+drop table analyst.delivery_leads
+create table analyst.delivery_leads
+as
+
+    select
+    distinct
+            d.driver_gk,
+            d.phone phone_number,
+            d.driver_name registration_name,
+
+            (case when d.driver_gk = prog.driver_gk then
+            (case when json_format(json_array_get(cast(source as json), 0)) like '%web%' then 'Plan-net'
+                when json_format(json_array_get(cast(source as json), 0)) like '%workle%' then 'Workle' end) end) external_source,
+            d.fleet_gk,
+
+            (case when d.driver_gk = ref.driver_gk then 'Reff'
+            when d.fleet_gk in (200014202,200016265,200016266,200016267,200016359,200016361) then 'Gorizont'
+            when d.fleet_gk = 200017083 then 'Scouts'
+            else 'Fleet' end) source,
+
+            d.registration_date_key, d.ftp_date_key first_ftr,
+            max(case when ride_type = 'ReFTRD' then rftr.date_key end) reFTR,
+            max(case when d.driver_gk = prog.driver_gk then date(lead_date) end) as "external_source_lead_date"
+
+        from emilia_gettdwh.dwh_dim_drivers_v d
+        left join bp_ba.sm_ftr_reftr_drivers rftr on d.driver_gk = rftr.driver_gk
+            -- to filter by fleet name selecting only couriers
+            left join emilia_gettdwh.dwh_dim_vendors_v fl on d.fleet_gk = fl.vendor_gk
+            -- reff
+            left join
+            (
+                select
+                driver_gk, ftp_date_key between cast("start" as date) and cast("end" as date)
+                from emilia_gettdwh.dwh_dim_drivers_v d
+                left join "sheets"."default".ru_fleet_promo ref on cast(ref.fleet_gk as integer) = d.fleet_gk
+                where 1=1
+                -- select drivers who were led by reff
+                and ftp_date_key between cast("start" as date) and cast("end" as date)
+                and d.country_key = 2
+            ) ref on ref.driver_gk = d.driver_gk
+
+            -- external sources: workle, website etc. It's taken from GoogleSheet filled by Valera
+            left join
+            (
+                    select distinct d.phone phone_number, "name",
+                    d.driver_name registration_name,
+                    d.fleet_gk, driver_gk,
+                    vendor_name,
+                    --courier_details, request_id, leads.city,
+                    array_agg("source") source,
+                    max(date(lead_date)) as lead_date
+
+                    -- google sheet
+                    from sheets."default".delivery_courier_leads_new leads
+                    -- get info about drivers by their phones
+                    LEFT JOIN "emilia_gettdwh"."dwh_dim_drivers_v" d
+                        ON substring(d.phone, -10) = leads.phone_number
+                            and d.phone not in ('89999999999', '8', '')
+                            and country_key = 2
+                    left join emilia_gettdwh.dwh_dim_vendors_v fl on d.fleet_gk = fl.vendor_gk
+
+                    where "source" <> 'source' --filter the bug that occured because of union of tables in google sheet
+                    and phone_2 <> 'phone_2'
+                    and phone_number not in ('8', '', '9999999999', ' ', '3333333333', '2222222222') -- dummy phones
+                    and phone_number is not null
+                    and cast(lead_date as date) >= date'2020-07-01'
+                    group by 1,2,3,4,5,6
+            ) prog on prog.driver_gk = d.driver_gk
+
+
+            where 1=1
+            and d.phone is not null
+            and d.driver_gk <> 2000683923 -- some old bug
+            and fl.vendor_name like '%courier%'
+            and d.country_key = 2
+            and d.ftp_date_key >= date'2020-07-01'
+            group by 1,2,3,4,5,6,7,8;
+
+GRANT select on analyst.new_and_churned_couriers to ROLE public
