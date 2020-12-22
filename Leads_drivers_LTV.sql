9e3142475ee3e2c78e7a03d07e889800c59f7fcd
kseniya <kozlovakseniya97@gmsil.com>
2020-12-22 13:35:30 +0300

    Delivery_leads, reFTR, deliveries on Fact orders

--- Leads_drivers_LTV.sql
+++ Leads_drivers_LTV.sql
@@ -1,166 +1,44 @@
-
---from (
---1. Get the list of courier driver_gk who registered since 2020-07-01
-------- detect external sources - workle, Plan-net
-
-/*
-Owner - Ksenia Kozlova
-Cube Name - Leads_all_sources_1.11.20
-ID - 868A51A811EB1D1D0CB50080EFD535C4
-*/
-with leads as (
-    --select count( distinct driver_gk) from (
-    select
-    distinct d.driver_gk,
-            d.phone phone_number,
-            d.driver_name registration_name,
-            prog.courier_details, prog.request_id, prog.city, -- info about leads from workle, website, etc
-
-            (case when d.driver_gk = prog.driver_gk then prog.source end) external_source,
-            d.fleet_gk,
-
-            (case when d.driver_gk = ref.driver_gk then True else False end) is_reff,
-            d.fleet_gk in (200014202,200016265,200016266,200016267,200016359,200016361) is_agent,
-            d.fleet_gk = 200017083 is_scouts,
-
-            d.registration_date_key, d.ftp_date_key,
-            max(case when d.driver_gk = prog.driver_gk then date(lead_date) end) as "lead_date"
-
-        from emilia_gettdwh.dwh_dim_drivers_v d
-            -- to filter by fleet name selecting only couriers
-            left join emilia_gettdwh.dwh_dim_vendors_v fl on d.fleet_gk = fl.vendor_gk
-            -- reff
-            left join
-            (
-                select
-                driver_gk, ftp_date_key between cast("start" as date) and cast("end" as date)
-                from emilia_gettdwh.dwh_dim_drivers_v d
-                left join "sheets"."default".ru_fleet_promo ref on cast(ref.fleet_gk as integer) = d.fleet_gk
-                where 1=1
-                -- select drivers who were led by reff
-                and ftp_date_key between cast("start" as date) and cast("end" as date)
-                and d.country_key = 2
-            ) ref on ref.driver_gk = d.driver_gk
-
-            -- external sources: workle, website etc. It's taken from GoogleSheet filled by Valera
-            left join
-            (
-                    select distinct d.phone phone_number, "name",
-                    d.driver_name registration_name,
-                    d.fleet_gk, driver_gk,
-                    vendor_name,
-                    courier_details, request_id, leads.city,
-                    "source",
-                    max(date(lead_date)) as lead_date
-
-                    -- google sheet
-                    from sheets."default".delivery_courier_leads_new leads
-                    -- get info about drivers by their phones
-                    LEFT JOIN "emilia_gettdwh"."dwh_dim_drivers_v" d
-                        ON substring(d.phone, -10) = leads.phone_number
-                            and d.phone not in ('89999999999', '8', '')
-                            and country_key = 2
-                    left join emilia_gettdwh.dwh_dim_vendors_v fl on d.fleet_gk = fl.vendor_gk
-
-                    where "source" <> 'source' --filter the bug that occured because of union of tables in google sheet
-                    and phone_2 <> 'phone_2'
-                    and phone_number not in ('8', '', '9999999999', ' ', '3333333333', '2222222222') -- dummy phones
-                    and phone_number is not null
-                    and cast(lead_date as date) >= date'2020-07-01'
-
-                    group by 1,2,3,4,5,6,7,8,9,10
-            ) prog on prog.driver_gk = d.driver_gk
-
-
-            where 1=1
-            -- this string is logically correct but decrease number of active couriers among agents
-            --and substring (d.phone, -10) not in ('', '3333333333', '2222222222')
-            and d.phone is not null
-            and d.driver_gk <> 2000683923 -- some old bug
-            and fl.vendor_name like '%courier%'
-            and d.country_key = 2
-            and d.ftp_date_key >= date'2020-07-01'
-            group by 1,2,3,4,5,6,7,8,9,10,11,12,13
-           --) where is_reff = True
-            )
-
---2. Get activity information about drivers on the date_key level - TR, deliveries
------- determinate agents and reff drivers (on the level of the day as they can change fleets)
 select
--- driver info + date key
-distinct l.driver_gk,
+distinct
+l.driver_gk,
 l.registration_name,
 l.phone_number,
 fo.date_key,
 tp.timecategory,
 tp.period,
 tp.subperiod,
-        --which fleet was that day + fox bug with fleet -1 for orders
-(case when fo.fleet_gk = -1 then l.fleet_gk else fo.fleet_gk end) fleet_gk,
 fl.vendor_name,
-
--- raw agents & reff leads, not considering whether they've done ftr or not
-l.is_reff is_reff_original,
-l.is_agent is_agent_original,
-l.is_scouts,
-
---determination of the source by factual activity
-(case when fo.fleet_gk = -1 then l.fleet_gk else fo.fleet_gk end) = cast(ref.fleet_gk AS bigint) is_reff,
-(case when fo.fleet_gk = -1 then l.fleet_gk else fo.fleet_gk end) in (200014202,200016265,200016266,200016267,200016359,200016361) is_agent,
-l.registration_date_key, -- date of registration in our system
-l.lead_date, -- for external source
-l.external_source,
-l.courier_details, l.request_id, l.city, --external source information
-l.registration_date_key >= l.lead_date in_our_system_first,
-
+l.first_ftr, l.reftr,
+l.reftr is not null is_reftr,
+(case when date(l.reftr) is not null then date(l.reftr) else l.first_ftr end) ftr,
 -- activity
-fo.region_name,
+fo.city_name,
 fo.TR,
-fo.orders deliveries_OF,
-md.deliveries deliveries_NF,
-md.journeys journeys_NF,
-l.ftp_date_key, -- from dim drivers
+fo.of_orders deliveries_OF,
+fo.nf_orders deliveries_NF,
+fo.journeys_total journeys_total,
 
+coalesce(fo.of_orders,0)  + coalesce(fo.nf_orders,0) deliveries,
 count(fo.date_key) over(partition by l.driver_gk) LT,
-min(fo.date_key) over(partition by l.driver_gk) FTR,
-max(fo.date_key) over(partition by l.driver_gk) last_date,
+max(fo.date_key) over(partition by l.driver_gk) last_date
 
--- source lable
-(case when (l.is_agent = True and l.external_source is null) or
-        (l.registration_date_key <= l.lead_date and l.is_agent = True ) then 'Gorizont'
-        when (l.is_reff = True and l.external_source is null) or (
-        l.registration_date_key <= l.lead_date and l.is_reff = True) then 'Reff'
-        when l.external_source is not null then l.external_source
-        else 'Fleet' end) source_lable,
+from analyst.delivery_leads l
 
-(case when l.is_agent = True then 'Gorizont'
-        when l.is_reff = True then 'Reff'
-        when l.is_scouts = True then 'Scouts'
-        when l.external_source is not null then l.external_source
-        else 'Fleet' end) source_lable2
-
-
-from leads l
-
--- Deliveries OF, region name, TR, last_date, FTR
-left join --14 sec
-  (
-        --select count(distinct driver_gk) from (
-        select
+-- Deliveries, region name, TR, last_date, FTR
+left join
+(       select
         distinct driver_gk,
         date_key,
-        region_name,
-        fo.fleet_gk,
+        city_name,
         --finance
         (sum(customer_total_cost) - sum(driver_total_cost_inc_vat))
         + (sum(driver_total_commission_exc_vat)*(-1)) TR,
-        --count(distinct case when order_status_key = 7 then date_key end) LT_days,
-        -- ftr on any platform
-        --min(date_key) ftr,
-
         -- orders only on OF
         count(distinct case when ct.class_family <> 'Premium'
-         and ordering_corporate_account_gk <> 20004730 then order_gk end) orders
+         and ordering_corporate_account_gk <> 20004730 then  order_gk end) of_orders,
+        count(distinct case when ordering_corporate_account_gk = 20004730 then  order_gk end) nf_orders,
+        count(distinct case when ordering_corporate_account_gk <> 20004730 then  order_gk end) journeys_total
+
 
         from emilia_gettdwh.dwh_fact_orders_v fo
         left join emilia_gettdwh.dwh_dim_class_types_v AS ct
@@ -170,54 +48,24 @@ left join --14 sec
 
         where fo.lob_key in (5,6)
         and date_key >= date'2020-07-01'
-        and order_status_key = 7
+        --and order_status_key = 7
         and fo.country_key = 2
+        --and driver_gk = 2000747210
 
---         ---TODO Check
---         and driver_gk in (2000837187,2000837478,2000842055,2000843852,2000844651)
---         and date_key >= date'2020-10-01'
+        group by 1,2,3
 
-        --and fo.fleet_gk in (200014202,200016265,200016266,200016267,200016359,200016361)
-        group by 1,2,3,4
-        --)
-
-    ) fo on fo.driver_gk = l.driver_gk
-
--- Deliveries NF
-left join --2sec
-    (
-        select
-        distinct courier_gk,
-        date(created_at) date_key,
-        count(distinct delivery_gk) deliveries,
-        count(distinct journey_gk) journeys
-
-        from model_delivery.dwh_fact_deliveries_v
-
-        where
-        date(created_at) >= date'2020-07-01'
-        and delivery_status_id = 4
-
-        group by 1,2
-
-    ) md on md.courier_gk  = fo.driver_gk and md.date_key = fo.date_key
+    ) fo on l.driver_gk = fo.driver_gk
 
 -- timecategory
 LEFT JOIN  emilia_gettdwh.periods_v AS tp ON tp.hour_key = 0 and tp.date_key = fo.date_key
        and tp.timecategory IN ('2.Dates', '3.Weeks', '4.Months')
        and tp.timecategory is not null
-
-
--- reff
-left join "sheets"."default".ru_fleet_promo ref on fo.fleet_gk = cast(ref.fleet_gk as bigint)
 -- fleet names
-left join emilia_gettdwh.dwh_dim_vendors_v fl on fo.fleet_gk = fl.vendor_gk
-
-;
+left join emilia_gettdwh.dwh_dim_vendors_v fl on l.fleet_gk = fl.vendor_gk
 
 
 
-
+select * from model_delivery.dwh_dim_delivery_statuses_v
 --group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18;
 
 
