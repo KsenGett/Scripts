d1ec731bd0a1b2c879780bbc088eafe4bc26602e
kseniya <kozlovakseniya97@gmsil.com>
2021-01-19 14:09:28 +0300

    Cities was fixed

--- New drivers.sql
+++ New drivers.sql
@@ -1,5 +1,5 @@
-
-    select
+with main as (
+select
     date(rftr.ftr) as date_key,
     tp.timecategory,
     tp.subperiod,
@@ -11,8 +11,9 @@
                                   when  v.vendor_name like '%courier trike%' THEN 'e-bike'
                                   ELSE 'taxi' end) AS supply_type,
      v."vendor_name",
-     l.city_name,
+     fo.city_name,
      dd.driver_gk,
+     (case when dl.external_source is null then dl.source else external_source end )as lead_source ,
      fo.orders orders_OF,
     md.deliveries deliveries_NF,
     md.journeys journeys_NF,
@@ -20,9 +21,7 @@
 
 
     from "emilia_gettdwh"."dwh_dim_drivers_v" dd
-
     join "emilia_gettdwh"."dwh_dim_vendors_v" v on v.vendor_gk = dd.fleet_gk and vendor_name like '%courier%'
-    left join "emilia_gettdwh"."dwh_dim_locations" l on dd."primary_city_id" = l.city_id
     -- reftr
     left join (
 
@@ -42,31 +41,32 @@
       (
             select
             distinct driver_gk,
-            count(distinct order_gk) orders
+            l.city_name,
+            count(distinct case when ordering_corporate_account_gk <> 20004730 and
+             ct.class_family <> 'Premium' then order_gk end) orders
 
             from emilia_gettdwh.dwh_fact_orders_v fo
             left join emilia_gettdwh.dwh_dim_class_types_v AS ct
                 ON ct.class_type_key = fo.class_type_key
+            left join "emilia_gettdwh"."dwh_dim_locations" l on fo.origin_location_key = l.location_key
 
             where fo.lob_key in (5,6)
             --and date_key between current_date - interval '30' day and current_date
-            and ordering_corporate_account_gk <> 20004730
             and order_status_key = 7
             and fo.country_key = 2
-            and ct.class_family <> 'Premium'
 
-            group by 1
+            group by 1,2
 
         ) fo on fo.driver_gk = dd.driver_gk
 
     left join
         (
-            select distinct courier_gk,
+            select
+            distinct courier_gk,
             count(distinct delivery_gk) deliveries,
             count(distinct journey_gk) journeys
 
-            from model_delivery.dwh_fact_deliveries_v
-
+            from model_delivery.dwh_fact_deliveries_v fd
             where 1 = 1
             and date(scheduled_at) between current_date - interval '90' day and current_date
             and delivery_status_id = 4
@@ -78,13 +78,29 @@
     --change
     LEFT JOIN  data_vis.periods_v AS tp ON tp."hour_key" = 0 and tp.date_key = date(rftr.ftr)
     and tp.timecategory IN ('2.Dates', '3.Weeks', '4.Months', '5.Quarters', '7.Std Hours')
+    left join analyst.delivery_leads dl on dl.driver_gk = dd.driver_gk
 
     where 1=1
     and dd.country_key = 2
     --and dd.ftp_date_key between date '2020-01-01' and current_date - interval '1' day
     and tp.timecategory is not null
-    and date(rftr.ftr) >= date'2019-01-01'
+)
 
+(select
+city_name,
+count(distinct driver_gk)
+from main
+where date_key between date'2020-12-7' and date'2020-12-13'
+group by 1)
+
+
+
+select
+source ,
+count(distinct driver_gk)
+from analyst.delivery_leads
+where coalesce( date(reftr), first_ftr) = date'2021-1-17'
+group by 1;
 
 
 -- to check
@@ -101,5 +117,45 @@ with ftr as (
 select
 count(distinct driver_gk)
 from ftr
-where date(ftr) between date'2021-1-11' and date'2021-1-17'
+where date(ftr) between date'2020-12-7' and date'2020-12-13'; --w50
 
+
+
+
+/*
+Owner - Ekaterina Nesterova
+Cube Name - Churned Drivers
+ID - C09E509211EAF1493B290080EF15A834
+*/
+with main as (
+(
+select ltp_date_key+ interval '7' day as date_key,
+tp.timecategory,
+tp.subperiod,
+tp.period,
+tp.subperiod2 AS time_period,
+(CASE when v.vendor_name like '%courier car%' THEN 'PHV'
+                              when v.vendor_name like '%courier pedestrian%' THEN 'pedestrian'
+                              when v.vendor_name like '%courier scooter%' THEN 'scooter'
+                              when  v.vendor_name like '%courier trike%' THEN 'e-bike'
+                              ELSE 'taxi' end) AS supply_type,
+ v."vendor_name",
+ l.city_name,
+ driver_gk
+from "emilia_gettdwh"."dwh_dim_drivers_v" dd
+left join "emilia_gettdwh"."dwh_dim_vendors_v" v on v.vendor_gk = dd.fleet_gk
+left join "emilia_gettdwh"."dwh_dim_locations" l on dd."primary_city_id" = l.city_id
+LEFT JOIN  data_vis.periods_v AS tp ON tp."hour_key" = hour(dd."ltp_datetime") and tp.date_key = (ltp_date_key+ interval '7' day)
+and tp.timecategory IN ('2.Dates', '3.Weeks', '4.Months', '5.Quarters', '7.Std Hours')
+where vendor_name like '%courier%'
+and dd.country_key = 2
+and tp.timecategory is not null
+and "ltp_date_key" between date '2019-12-02' and current_date - interval '7' day)
+)
+(select
+date_key,
+count(distinct driver_gk)
+
+from main
+where date_key >= date'2021-1-1'
+group by 1);
