62c3f069b7abfc8ad73aa654f593489c6d0b9918
kseniya <kozlovakseniya97@gmsil.com>
2021-03-15 11:03:31 +0300

    stabilised

--- Leads_drivers_LTV.sql
+++ Leads_drivers_LTV.sql
@@ -1,79 +1,168 @@
+with check as (
+
+with main as (
+
+    select
+    distinct
+    l.driver_gk,
+    l.registration_name,
+    l.phone_number,
+    l.source,
+    fo.date_key,
+    tp.timecategory,
+    tp.period,
+    tp.subperiod,
+    fl.vendor_name,
+    l.first_ftr, l.reftr,
+    l.reftr is not null is_reftr,
+    (case when date(l.reftr) is not null then date(l.reftr) else date(l.first_ftr) end) ftr,
+    -- activity
+    fo.city_name,
+    fo.TR,
+    fo.of_orders deliveries_OF,
+    fo.nf_orders deliveries_NF,
+    fo.journeys_total journeys_total,
+
+    coalesce(fo.of_orders,0)  + coalesce(fo.nf_orders,0) deliveries,
+    count(fo.date_key) over(partition by l.driver_gk) LT,
+    max(fo.date_key) over(partition by l.driver_gk) last_date
+
+    from
+        (
+
+        select
+        distinct
+                d.driver_gk,
+                d.phone phone_number,
+                d.driver_name registration_name,
+                d.fleet_gk,
+
+                (case when d.driver_gk = ref.driver_gk then 'Reff'
+                when d.fleet_gk in (200014202,200016265,200016266,200016267,200016359,200016361) then 'Gorizont'
+                when d.fleet_gk in (200017083,200017177,200017412,200017342,200017205,200017203, 200017524,200017523,200017517,200017430) then 'Scouts'
+                when d.fleet_gk = 200017111 then 'D_Uspekha'
+                else 'Fleet' end) source,
+
+                d.registration_date_key,
+                nullif(d.ftp_date_key, date'1900-1-1') first_ftr,
+                max(case when ride_type = 'ReFTRD' then date(rftr.date_key) end) reFTR
+
+            from emilia_gettdwh.dwh_dim_drivers_v d
+            left join bp_ba.sm_ftr_reftr_drivers rftr on d.driver_gk = rftr.driver_gk
+                -- to filter by fleet name selecting only couriers
+                left join emilia_gettdwh.dwh_dim_vendors_v fl on d.fleet_gk = fl.vendor_gk
+                -- reff
+                left join
+                (
+                    select
+                    d.driver_gk,
+                    ( case when date(rftr.date_key) is null then d.ftp_date_key else date(rftr.date_key) end)
+                     between cast("start" as date) and cast("end" as date)
+
+                    from emilia_gettdwh.dwh_dim_drivers_v d
+                    left join bp_ba.sm_ftr_reftr_drivers rftr on d.driver_gk = rftr.driver_gk
+                    left join "sheets"."default".ru_fleet_promo ref on cast(ref.fleet_gk as integer) = d.fleet_gk
+
+                    where 1=1
+                    -- select drivers who were led by reff
+                    and (
+                        (( case when date(rftr.date_key) is null then d.ftp_date_key else date(rftr.date_key) end)
+                                                between cast("start" as date) and cast("end" as date))
+                        or (d.registration_date_key between cast("start" as date) and cast("end" as date))
+                            )
+
+                    and d.country_key = 2
+
+                ) ref on ref.driver_gk = d.driver_gk
+
+                where 1=1
+                and d.phone is not null
+                and d.driver_gk <> 2000683923 -- some old bug
+                and d.is_courier = 1
+                and d.country_key = 2
+                --and d.registration_date_key >= date'2020-07-01'
+                group by 1,2,3,4,5,6,7
+
+            ) l
+
+    -- Deliveries, region name, TR, last_date, FTR
+    left join
+    (       select
+            distinct driver_gk,
+            date_key,
+            city_name,
+            --finance
+            (sum(customer_total_cost) - sum(driver_total_cost_inc_vat))
+            + (sum(driver_total_commission_exc_vat)*(-1)) TR,
+            -- orders only on OF
+            count(distinct case when ct.class_family <> 'Premium'
+             and ordering_corporate_account_gk <> 20004730 then  order_gk end) of_orders,
+            count(distinct case when ordering_corporate_account_gk = 20004730 then  order_gk end) nf_orders,
+            count(distinct case when ordering_corporate_account_gk <> 20004730 then  order_gk end) journeys_total
+
+
+            from emilia_gettdwh.dwh_fact_orders_v fo
+            left join emilia_gettdwh.dwh_dim_class_types_v AS ct
+                ON ct.class_type_key = fo.class_type_key
+            left join emilia_gettdwh.dwh_dim_locations_v loc on
+                fo.origin_location_key = loc.location_key and loc.country_id = 2
+
+            where fo.lob_key in (5,6)
+            and date_key between current_date - interval '6' month and current_date
+            --and order_status_key = 7
+            and fo.country_key = 2
+            --and driver_gk = 2000747210
+
+            group by 1,2,3
+
+        ) fo on l.driver_gk = fo.driver_gk
+
+    -- timecategory
+    LEFT JOIN  emilia_gettdwh.periods_v AS tp ON tp.hour_key = 0 and tp.date_key = fo.date_key
+           and tp.timecategory IN ('2.Dates', '3.Weeks', '4.Months')
+           and tp.timecategory is not null
+    -- fleet names
+    left join emilia_gettdwh.dwh_dim_vendors_v fl on l.fleet_gk = fl.vendor_gk
+    )
+
+(
 select
-distinct
-l.driver_gk,
-l.registration_name,
-l.phone_number,
-fo.date_key,
-tp.timecategory,
-tp.period,
-tp.subperiod,
-fl.vendor_name,
-l.first_ftr, l.reftr,
-l.reftr is not null is_reftr,
-(case when date(l.reftr) is not null then date(l.reftr) else l.first_ftr end) ftr,
--- activity
-fo.city_name,
-fo.TR,
-fo.of_orders deliveries_OF,
-fo.nf_orders deliveries_NF,
-fo.journeys_total journeys_total,
-
-coalesce(fo.of_orders,0)  + coalesce(fo.nf_orders,0) deliveries,
-count(fo.date_key) over(partition by l.driver_gk) LT,
-max(fo.date_key) over(partition by l.driver_gk) last_date
-
-from analyst.delivery_leads l
-
--- Deliveries, region name, TR, last_date, FTR
-left join
-(       select
-        distinct driver_gk,
-        date_key,
-        city_name,
-        --finance
-        (sum(customer_total_cost) - sum(driver_total_cost_inc_vat))
-        + (sum(driver_total_commission_exc_vat)*(-1)) TR,
-        -- orders only on OF
-        count(distinct case when ct.class_family <> 'Premium'
-         and ordering_corporate_account_gk <> 20004730 then  order_gk end) of_orders,
-        count(distinct case when ordering_corporate_account_gk = 20004730 then  order_gk end) nf_orders,
-        count(distinct case when ordering_corporate_account_gk <> 20004730 then  order_gk end) journeys_total
+*
+from main
+where ftr between current_date - interval '6' month and current_date)
+)
+-- to check
+select
+date_key,
+count(distinct driver_gk)
+from check
+where timecategory = '2.Dates'
+and date_key = ftr
+and date_key > date'2021-3-2'
+group by 1
 
 
-        from emilia_gettdwh.dwh_fact_orders_v fo
-        left join emilia_gettdwh.dwh_dim_class_types_v AS ct
-            ON ct.class_type_key = fo.class_type_key
-        left join emilia_gettdwh.dwh_dim_locations_v loc on
-            fo.origin_location_key = loc.location_key and loc.country_id = 2
-
-        where fo.lob_key in (5,6)
-        and date_key >= date'2020-07-01'
-        --and order_status_key = 7
-        and fo.country_key = 2
-        --and driver_gk = 2000747210
-
-        group by 1,2,3
-
-    ) fo on l.driver_gk = fo.driver_gk
-
--- timecategory
-LEFT JOIN  emilia_gettdwh.periods_v AS tp ON tp.hour_key = 0 and tp.date_key = fo.date_key
-       and tp.timecategory IN ('2.Dates', '3.Weeks', '4.Months')
-       and tp.timecategory is not null
--- fleet names
-left join emilia_gettdwh.dwh_dim_vendors_v fl on l.fleet_gk = fl.vendor_gk
 
 
+select count(distinct driver_gk)
+from analyst.delivery_leads
+where date(reftr) between date'2020-12-14' and date'2020-12-20' -- w51
+
+
+select count(distinct d.driver_gk)
+from emilia_gettdwh.dwh_dim_drivers_v d
+left join emilia_gettdwh.dwh_dim_vendors_v fl on d.fleet_gk = fl.vendor_gk
+left join bp_ba.sm_ftr_reftr_drivers rftr on d.driver_gk = rftr.driver_gk
+
+where d.country_key = 2
+and vendor_name like '%courier%'
+and date(rftr.date_key) between date'2020-12-14' and date'2020-12-20'
+and ride_type = 'ReFTRD'
 
 select * from model_delivery.dwh_dim_delivery_statuses_v
 --group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18;
 
 
--- TODO check number of agents with dim_drivers/Fo. - DONE = 841 with strange phone numbers and
---  834 without, raw number of agent leads = 1444
-
--- TODO check number of reff = 3154 leads, 1942 working
---Agent original = 756 who registered in oct, and 403 who worked
 
 
 
